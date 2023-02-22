4cdf39c8fb58bb834d4ebc25c4c6c27450df55a4
Rene Leveille <rene.leveille@agilebits.com>
2023-02-22 17:32:51 -0500

    Add passkey-types crate level documentation

--- passkey-types/src/lib.rs
+++ passkey-types/src/lib.rs
@@ -1,8 +1,32 @@
 //! # Passkey Types
 //!
-//! Rust type definitions for the `webauthn` and `CTAP` specifications.
+//! [![github]](https://github.com/1Password/passkey-rs/tree/main/passkey-types)
+//! [![version]](https://crates.io/crates/passkey-types)
+//! [![documentation]](crate)
 //!
-//! Coming Soon
+//! This crate contains the types defined in both the [WebAuthn Level 3] and [CTAP 2.0]
+//! specifications for the operations they define. They are each seperated in their own modules.
+//!
+//! ## Webauthn
+//!
+//! In [this](webauthn) module the type names mirror exactly those in the specifications for ease of
+//! navigation. They are defined in a way that allows interoperability with the web types directly
+//! as well as the [JSON encoding] for over network communication.
+//!
+//! ## CTAP 2
+//!
+//! In [this](ctap2) module, seeing as the method inputs are not given explicit names, the `Request` and
+//! `Response` types are defined in seperate modules for each operation. These types make use of the
+//! same data structures from the [`webauthn`] module. In some cases though, the types have
+//! different constraits regarding required and optional fields, in which case it is re-defined in
+//! the [`ctap2`] module along with a [`TryFrom`] implementation in either direction.
+//!
+//! [github]: https://img.shields.io/badge/GitHub-1Password%2Fpasskey--rs%2Fpasskey--types-informational?logo=github&style=flat
+//! [version]: https://img.shields.io/crates/v/passkey-types?logo=rust&style=flat
+//! [documentation]: https://img.shields.io/docsrs/passkey-types/latest?logo=docs.rs&style=flat
+//! [WebAuthn Level 3]: https://w3c.github.io/webauthn/
+//! [CTAP 2.0]: https://fidoalliance.org/specs/fido-v2.0-ps-20190130/fido-client-to-authenticator-protocol-v2.0-ps-20190130.html
+//! [JSON encoding]: https://w3c.github.io/webauthn/#typedefdef-publickeycredentialjson
 
 #[macro_use]
 mod utils;
