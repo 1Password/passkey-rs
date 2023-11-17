3e6469d24658f1cd5580c45f4b60e1f99cfe9a60
Rene Leveille <rene.leveille@agilebits.com>
2023-11-17 11:48:52 -0500

    use into for hashing

--- passkey-types/src/utils/crypto.rs
+++ passkey-types/src/utils/crypto.rs
@@ -5,5 +5,5 @@ use sha2::{Digest, Sha256};
 /// Compute the SHA-256 of the given `data`.
 pub fn sha256(data: &[u8]) -> [u8; 32] {
     // SAFETY: sha256 always gives a 32 byte array
-    Sha256::digest(data).try_into().unwrap()
+    Sha256::digest(data).into()
 }
