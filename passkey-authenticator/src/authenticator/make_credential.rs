f75da724891d378e304e8770dbde5b2c173fa1a7
alan <652732310@qq.com>
2025-06-03 10:05:39 +0800

    fix CI

--- passkey-authenticator/src/authenticator/make_credential.rs
+++ passkey-authenticator/src/authenticator/make_credential.rs
@@ -33,14 +33,13 @@ where
             .as_ref()
             .filter(|list| !list.is_empty())
             .is_some()
-        {
-            if !self
+            && !self
                 .store()
                 .find_credentials(input.exclude_list.as_deref(), &input.rp.id)
-                .await?.is_empty()
-            {
-                return Err(Ctap2Error::CredentialExcluded.into());
-            }
+                .await?
+                .is_empty()
+        {
+            return Err(Ctap2Error::CredentialExcluded.into());
         }
 
         // 2. If the pubKeyCredParams parameter does not contain a valid COSEAlgorithmIdentifier
